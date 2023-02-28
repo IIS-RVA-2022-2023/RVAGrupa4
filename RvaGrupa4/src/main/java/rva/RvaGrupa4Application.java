3be35c670dc25526cb77584b3a85dae505dab3b2
dejanagladic <dejanagladic@AR1-Asistent.ftn.ns>
2023-02-28 08:51:50 +0100

    Kreiran je inicijalni projekat

---
+++ RvaGrupa4/src/main/java/rva/RvaGrupa4Application.java
@@ -0,0 +1,13 @@
+package rva;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RvaGrupa4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RvaGrupa4Application.class, args);
+	}
+
+}
